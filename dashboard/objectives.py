90bc83ab3a840d06e3eaaa08da3c80895e7663dc
Edoardo Zoni <ezoni@lbl.gov>
2024-11-15 14:45:21 -0800

    Refactoring: new classes for parameters, objectives

---
+++ dashboard/objectives.py
@@ -0,0 +1,23 @@
+class Objectives:
+    def __init__(self, server, model, output_variables):
+        # FIXME generalize for multiple objectives
+        assert len(output_variables) == 1, "number of objectives > 1 not supported"
+        # Trame state and controller
+        self.__state = server.state
+        self.__ctrl = server.controller
+        # save PyTorch model
+        self.__model = model
+        # define state variables
+        self.__state.objectives = dict()
+        for _, objective_dict in output_variables.items():
+            key = objective_dict["name"]
+            self.__state.objectives[key] = model.evaluate(self.__state.parameters)
+
+    def get(self):
+        return self.__state.objectives
+
+    def update(self):
+        for key in self.__state.objectives.keys():
+            self.__state.objectives[key] = self.__model.evaluate(self.__state.parameters)
+        # push again at flush time
+        self.__state.dirty("objectives")
