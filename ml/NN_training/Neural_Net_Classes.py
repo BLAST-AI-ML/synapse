ef95ce7dbe86cad32cccbc5a72fcc968f3816f47
RevathiJambunathan <revanathan@gmail.com>
2024-11-18 20:54:26 -0500

    Adding calibration class

--- ml/NN_training/Neural_Net_Classes.py
+++ ml/NN_training/Neural_Net_Classes.py
@@ -6,6 +6,100 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
+
+class Calibration(nn.Module):
+    def __init__(self, base_model, criterion, calibration_layer, learning_rate=0.001, patience=100, factor=0.5, threshold=1e-4):
+        super(Calibration, self).__init__()
+        if not isinstance(base_model, nn.Module):
+            raise TypeError("Base model must be an instance of nn.Module")
+        self.model =base_model
+        self.calibration_layer = calibration_layer
+        self.criterion = criterion
+        # Freeze the existing network
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Define an optimizer for the linear layer
+        self.optimizer = optim.Adam(calibration_layer.parameters(), lr=learning_rate)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, 'min',
+                                           factor=factor, patience=patience, threshold=threshold)
+        self.loss_data = {
+            'loss':[],
+            'epoch_count':[]
+        }
+
+    def forward(self, x):
+        x = self.model(x)
+        x = self.calibration_layer(x)
+        return x
+
+    def train(self, inputs, targets,num_epochs=10000):
+        for epoch in range(num_epochs):
+            self.optimizer.zero_grad()
+            outputs = self.model(inputs)
+            calibrated_outputs = self.calibration_layer(outputs)
+            loss = self.criterion(calibrated_outputs, targets)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            current_loss = loss.item()
+            self.loss_data['loss'].append(loss.detach().numpy())
+            self.loss_data['epoch_count'].append(epoch)
+            self.scheduler.step(current_loss)
+            if(epoch+1) % (num_epochs/10) == 0:
+                print(f'Comb NN: Epoch [{epoch+1}/{num_epochs}], Loss:{loss.item():.6f}')
+                print(f'Calibration Layer Weight: {self.calibration_layer.weight.item()}')
+                print(f'Calibration Layer Bias: {self.calibration_layer.bias.item()}')
+
+
+    def predict(self, inputs):
+        '''
+        args:
+            tensor inputs
+        returns:
+            numpy array with predictions
+        '''
+        with torch.no_grad():
+            output = self.model(inputs)
+            calibrated_test_outputs = self.calibration_layer(output)
+            predictions = calibrated_test_outputs.detach().numpy()
+        return predictions
+
+    def test_model(self, inputs, outputs):
+        '''
+        args:
+            tensor inputs: an input dataset to pass through NN and test
+            tensor outputs: an output dataset to pass through NN
+        '''
+
+        self.eval()
+        with torch.no_grad():
+            predictions = self(inputs)
+            loss = self.criterion(predictions, outputs).item()
+
+
+    def plot_loss(self, filename=None):
+        '''
+        Args:
+            if a string is provided it will save the plot with the given name
+        return:
+            displays epochs vs loss graph
+        '''
+        fig, ax = plt.subplots()
+        ax.plot(self.loss_data['epoch_count'], self.loss_data['loss'], label='loss')
+        plt.title('epochs vs loss')
+        plt.xlabel('epochs')
+        plt.ylabel('loss')
+
+        if filename:
+            plt.savefig(filename+'.png')
+        else:
+            plt.show()
+
+
+
+
+
 class NN(nn.Module):
     '''
     5 hidden layer
