a5a45665d65d649b67f02452679ef76e3d48c46c
Remi Lehe <remi.lehe@normalesup.org>
2025-02-28 08:50:35 -0800

    More analysis

--- simulation_data/acave/templates/analyze_simulation.py
+++ simulation_data/acave/templates/analyze_simulation.py
@@ -4,25 +4,25 @@ import os
 import json
 import pymongo
 from openpmd_viewer.addons import LpaDiagnostics
+from datetime import datetime
 
 # Get current directory
 data_directory = os.path.join( os.getcwd(), 'diags/diag' )
 ts = LpaDiagnostics( data_directory )
 
-# Compute average wavelength at the last iteration
-last_iteration = 3500
-S, info = ts.get_laser_spectral_intensity(iteration=last_iteration, pol='x')
-lambda_avg = np.average( 2*np.pi/info.k[1:], weight=S[1:] )
-
-# Log results to the database
-
 # Load input parameters
 with open('input_params.json') as file:
     data = json.load(file)
+# Additional metadata
+data['experiment_flag'] = 0
+data['date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+data['data_directory'] = data_directory
 
-# Add the mean wavelength
-
-# Add date and folder
+# Compute average wavelength at the last iteration and add it
+last_iteration = 3500
+S, info = ts.get_laser_spectral_intensity(iteration=last_iteration, pol='x')
+lambda_avg = np.average( 2*np.pi/info.k[1:], weight=S[1:] )
+data['kHz_thorlab_spectrometer mean_wavelength'] = lambda_avg
 
 # Write to the data base
 db = pymongo.MongoClient(
