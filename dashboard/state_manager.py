b698c7bc1241ecbe66d70b67520d416d88be7e31
Edoardo Zoni <59625522+EZoni@users.noreply.github.com>
2025-05-28 12:15:12 -0700

    GUI: Updates & Fixes (#108)

--- dashboard/state_manager.py
+++ dashboard/state_manager.py
@@ -1,6 +1,4 @@
-#from datetime import datetime, timedelta
 import copy
-import os
 import pandas as pd
 from trame.app import get_server
 
@@ -8,41 +6,24 @@ server = get_server(client_type="vue2")
 state = server.state
 ctrl = server.controller
 
-def init_startup():
+def initialize_state():
     """
     Helper function to initialize state variabes needed at startup.
     """
     print(f"Initializing state variables at startup...")
-    state.nersc_route_built = False
-    state.ui_layout_built = False
-    # need separate variables to track changes in state.experiment and
-    # state.model_type, which trigger re-initialization, to avoid multiple
-    # reactive functions listening to changes in those variables (the order
-    # of execution of such reactive functions cannot be prescribed)
+    # experiment and simulation data (pandas dataframes serialized)
+    state.exp_data_serialized = pd.DataFrame().to_json(default_handler=str)
+    state.sim_data_serialized = pd.DataFrame().to_json(default_handler=str)
+    # experiment and model type
     state.experiment = "qed_ip2"
-    state.experiment_old = copy.deepcopy(state.experiment)
-    state.experiment_changed = False
     state.model_type = "Neural Network"
-    state.model_type_changed = False
-    state.model_type_old = copy.deepcopy(state.model_type)
     # opacity
     state.opacity = 0.05
+    # calibration
+    state.calibrate = True
     # Superfacility API
+    state.sfapi_client_id = None
     state.sfapi_key = None
     state.sfapi_key_dict = None
     state.sfapi_key_expiration = "Unavailable"
-    state.sfapi_client_id = None
     state.perlmutter_status = "Unavailable"
-
-def init_runtime():
-    """
-    Helper function to (re-)initialize state variables at runtime.
-    """
-    print(f"Initializing state variables at runtime...")
-    # serialized data
-    state.exp_data = pd.DataFrame().to_json(default_handler=str)
-    state.sim_data = pd.DataFrame().to_json(default_handler=str)
-    # calibration
-    state.is_calibrated = False
-    # parameters and objectives state variables are initialized
-    # in the respective manager class constructors
