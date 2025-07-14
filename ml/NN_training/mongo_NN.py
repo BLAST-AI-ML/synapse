50581ef67088621df550c80e7902c07328cb0919
Ethan Rodriguez <erodriguez133@lbl.gov>
2025-07-14 14:07:31 -0500

    Added File for Creating and training ensemble (#152)
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c3s3b1n0h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c2s2b0n0h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c2s2b0n1h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Revathi  Jambunathan <41089244+RevathiJambunathan@users.noreply.github.com>
    Co-authored-by: Ethan Joseph Rodriguez <erod@login08.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c4s3b1n0h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c2s4b0n1h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c2s7b0n1h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@login29.chn.perlmutter.nersc.gov>
    Co-authored-by: Remi Lehe <remi.lehe@normalesup.org>
    Co-authored-by: Ethan Joseph Rodriguez <erod@login26.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Joseph Rodriguez <erod@x1004c2s6b1n0h0.chn.perlmutter.nersc.gov>
    Co-authored-by: Ethan Rodriguez <erodriguez133@csit-8027950.dhcp.lbl.gov>
    Co-authored-by: RevathiJambunathan <revanathan@gmail.com>
    Co-authored-by: Ethan Joseph Rodriguez <erod@login37.chn.perlmutter.nersc.gov>

--- ml/NN_training/mongo_NN.py
+++ ml/NN_training/mongo_NN.py
@@ -3,37 +3,55 @@
 ## in a database using PyMongo
 ## Author : Revathi Jambunathan
 ## Date : January, 2025
+import time
 
 import argparse
 import torch
 from botorch.models.transforms.input import AffineInputTransform
+from botorch.models import MultiTaskGP, SingleTaskGP
+from botorch.fit import fit_gpytorch_mll
+from gpytorch.kernels import ScaleKernel, MaternKernel
 import pymongo
-import pandas as pd
 import os
 import re
 import yaml
 from lume_model.models import TorchModel
+from lume_model.models.ensemble import NNEnsemble
+from lume_model.models.gp_model import GPModel
 from lume_model.variables import ScalarVariable
+from lume_model.variables import DistributionVariable
 from sklearn.model_selection import train_test_split
 import sys
+import pandas as pd
+from gpytorch.mlls import ExactMarginalLogLikelihood
 
 
+############################################
+# Get command line arguments
+############################################
 # define parser
 parser = argparse.ArgumentParser()
-# add argument
 parser.add_argument(
     "--experiment",
     help="name/tag of the experiment",
     type=str,
     required=True,
 )
-# parse arguments
+parser.add_argument(
+    "--model",
+    help="Choose to train a model between GP, NN, or ensemble_NN",
+    required=True
+)
 args = parser.parse_args()
-
-# Select experiment for which we are training a model
 experiment = args.experiment
+model_type = args.model
+start_time = time.time()
+if model_type not in ['NN', 'ensemble_NN', 'GP']:
+    raise ValueError(f"Invalid model type: {model_type}")
 
+###############################################
 # Open credential file for database
+###############################################
 with open(os.path.join(os.getenv('HOME'), 'db.profile')) as f:
     db_profile = f.read()
 
@@ -68,15 +86,19 @@ for _, value in simulation_calibration.items():
     exp_name = value["depends_on"]
     df_sim[exp_name] = df_sim[sim_name] / value["alpha"] + value["beta"]
 
-#Split exp and sim data into training and validation data with 80:20 ratio, selected randomly
-exp_train_df, exp_val_df = train_test_split(df_exp, test_size=0.2, random_state=None, shuffle=True)# 20% of the data will go in validation test, no fixing the random_state will ensure the seed is different everytime, data will be shuffled randomly before splitting
-sim_train_df, sim_val_df = train_test_split(df_sim, test_size=0.2, random_state=None, shuffle=True)
-
 # Concatenate experimental and simulation data for training and validation
 variables = input_names + output_names + ['experiment_flag']
-df_train = pd.concat( (exp_train_df[variables], sim_train_df[variables]) )
-df_val = pd.concat( (exp_val_df[variables], sim_val_df[variables]) )
+if model_type != 'GP':
+    #Split exp and sim data into training and validation data with 80:20 ratio, selected randomly
+    exp_train_df, exp_val_df = train_test_split(df_exp, test_size=0.2, random_state=None, shuffle=True)# 20% of the data will go in validation test, no fixing the 
+    sim_train_df, sim_val_df = train_test_split(df_sim, test_size=0.2, random_state=None, shuffle=True)#random_state will ensure the seed is different everytime, data will be shuffled randomly before splitting
+    df_train = pd.concat( (exp_train_df[variables], sim_train_df[variables]) )
+    df_val = pd.concat( (exp_val_df[variables], sim_val_df[variables]) )
 
+else:
+    # No split: all the data is training data
+    df_train = pd.concate( (df_exp[variables], df_sim[variables]) )
+    
 # Normalize with Affine Input Transformer
 # Define the input and output normalizations
 X_train = torch.tensor( df_train[ input_names ].values, dtype=torch.float )
@@ -92,19 +114,6 @@ output_transform = AffineInputTransform(
     offset=y_train.mean(axis=0)
 )
 
-X_val = torch.tensor( df_val[ input_names ].values, dtype=torch.float )
-input_transform = AffineInputTransform(
-    len(input_names),
-    coefficient=X_val.std(axis=0),
-    offset=X_val.mean(axis=0)
-)
-y_val = torch.tensor( df_val[ output_names ].values, dtype=torch.float )
-output_transform = AffineInputTransform(
-    len(output_names),
-    coefficient=y_val.std(axis=0),
-    offset=y_val.mean(axis=0)
-)
-
 # Apply normalization to the training data set
 norm_df_train = df_train.copy()
 norm_df_train[input_names] = input_transform( torch.tensor( df_train[input_names].values ) )
@@ -115,60 +124,172 @@ norm_expt_outputs_train = torch.tensor( norm_df_train[norm_df_train.experiment_f
 norm_sim_inputs_train = torch.tensor( norm_df_train[norm_df_train.experiment_flag==0][input_names].values, dtype=torch.float)
 norm_sim_outputs_train = torch.tensor( norm_df_train[norm_df_train.experiment_flag==0][output_names].values, dtype=torch.float)
 
-# Apply normalization to the validation data set
-norm_df_val = df_val.copy()
-norm_df_val[input_names] = input_transform( torch.tensor( df_val[input_names].values ) )
-norm_df_val[output_names] = output_transform( torch.tensor( df_val[output_names].values ) )
 
-norm_expt_inputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==1][input_names].values, dtype=torch.float)
-norm_expt_outputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==1][output_names].values, dtype=torch.float)
-norm_sim_inputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==0][input_names].values, dtype=torch.float)
-norm_sim_outputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==0][output_names].values, dtype=torch.float)
+######################################################
+# Neural Net and Ensemble Creation and training
+######################################################
+if model_type != 'GP':
+    # Saving the Lume Model - TO do for combined NN
+    path_to_save = path_to_IFE_sf_src+'/ml/saved_models/NN_training/'
+    ##############################
+    #Early Stopping and validation
+    ##############################
+    X_val = torch.tensor( df_val[ input_names ].values, dtype=torch.float )
+    input_transform = AffineInputTransform(
+        len(input_names),
+        coefficient=X_val.std(axis=0),
+        offset=X_val.mean(axis=0)
+    )
+    y_val = torch.tensor( df_val[ output_names ].values, dtype=torch.float )
+    output_transform = AffineInputTransform(
+        len(output_names),
+        coefficient=y_val.std(axis=0),
+        offset=y_val.mean(axis=0)
+    )
+
+    # Apply normalization to the validation data set
+    norm_df_val = df_val.copy()
+    norm_df_val[input_names] = input_transform( torch.tensor( df_val[input_names].values ) )
+    norm_df_val[output_names] = output_transform( torch.tensor( df_val[output_names].values ) )
+
+    norm_expt_inputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==1][input_names].values, dtype=torch.float)
+    norm_expt_outputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==1][output_names].values, dtype=torch.float)
+    norm_sim_inputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==0][input_names].values, dtype=torch.float)
+    norm_sim_outputs_val = torch.tensor( norm_df_val[norm_df_val.experiment_flag==0][output_names].values, dtype=torch.float)
 
 
-# Train combined NN
-calibrated_nn = CombinedNN( len(input_names), len(output_names), learning_rate=0.0005)
-calibrated_nn.train_model(
-    norm_sim_inputs_train, norm_sim_outputs_train,
-    norm_expt_inputs_train, norm_expt_outputs_train,
-    norm_sim_inputs_val, norm_sim_outputs_val,
-    norm_expt_inputs_val, norm_expt_outputs_val,
-    num_epochs=20000)
+    NN_start_time = time.time()
+    if model_type == 'NN':
+        num_models = 1
+    elif model_type == 'ensemble_NN':
+        num_models = 10
+
+    ensemble = []
+    for i in range(num_models):
+        model = CombinedNN(len(input_names), len(output_names), learning_rate=0.0001)
+        model.train_model(
+            norm_sim_inputs_train, norm_sim_outputs_train,
+            norm_expt_inputs_train, norm_expt_outputs_train,
+            norm_sim_inputs_val, norm_sim_outputs_val,
+            norm_expt_inputs_val, norm_expt_outputs_val,
+            num_epochs=20000)
+        print(f'Model_{i+1} trained')
+        ensemble.append(model)
 
 
-# Saving the Lume Model - TO do for combined NN
-calibration_transform = AffineInputTransform(
-    len(output_names),
-    coefficient=calibrated_nn.sim_to_exp_calibration.weight.clone(),
-    offset=calibrated_nn.sim_to_exp_calibration.bias.clone() )
+    torch_models = []
+    for model_nn in ensemble:
+        calibration_transform = AffineInputTransform(
+            len(output_names),
+            coefficient=model_nn.sim_to_exp_calibration.weight.clone(),
+            offset=model_nn.sim_to_exp_calibration.bias.clone() )
 
-# Fix mismatch in name between the config file and the expected lume-model format
-for k in input_variables:
-    print(input_variables[k])
-    input_variables[k]['default_value'] = input_variables[k]['default']
-    del input_variables[k]['default']
+        # Fix mismatch in name between the config file and the expected lume-model format
+        for k in input_variables:
+            print(input_variables[k])
+            input_variables[k]['default_value'] = input_variables[k]['default']
 
-model = TorchModel(
-    model=calibrated_nn,
-    input_variables=[ ScalarVariable(**input_variables[k]) for k in input_variables.keys() ],
-    output_variables=[ ScalarVariable(**output_variables[k]) for k in output_variables.keys() ],
-    input_transformers=[input_transform],
-    output_transformers=[calibration_transform,output_transform] # saving calibration before normalization
-)
+        torch_model = TorchModel(
+            model=model_nn,
+            input_variables=[ ScalarVariable(**input_variables[k]) for k in input_variables.keys() ],
+            output_variables=[ ScalarVariable(**output_variables[k]) for k in output_variables.keys() ],
+            input_transformers=[input_transform],
+            output_transformers=[calibration_transform,output_transform] # saving calibration before normalization
+        )
+        if num_models == 1:
+            #Save single NN and break
+            torch_model.dump( file=os.path.join(path_to_save, experiment+'.yml'), save_jit=True )
+            print(f"Model saved to {path_to_save}")
+            #torch_model.dump(file=os.path.join(models_path, experiment+'NN.yml'), save_jit=True)
+            end_time = time.time()
+            break
+        torch_models.append(torch_model)
 
-path_to_save = path_to_IFE_sf_src+'/ml/saved_models/NN_training/'
-model.dump( file=os.path.join(path_to_save, experiment+'.yml'), save_jit=True )
-print(f"Model saved to {path_to_save}")
+    #Save Ensemble
+    if num_models > 1:
+        nn_ensemble = NNEnsemble(
+        models=torch_models,
+        input_variables=[ ScalarVariable(**input_variables[k]) for k in input_variables.keys() ],
+        output_variables=[ DistributionVariable(**output_variables[k]) for k in output_variables.keys() ]
+        )
+        nn_ensemble.dump( file=os.path.join(path_to_save, experiment+'ensemble.yml'), save_jit=True )
+        end_time = time.time()
+
+    elapsed_time = end_time - start_time
+    data_time = NN_start_time - start_time
+    NN_time = end_time - NN_start_time
+    print(f"Total time taken: {elapsed_time:.2f} seconds")
+    print(f"Data prep time taken: {data_time:.2f} seconds")
+    print(f"NN time taken: {NN_time:.2f} seconds")
+
+###############################################################
+# Guassian Process Creation and training
+###############################################################
+else:
+    if experiment != 'acave':
+        gp_model = MultiTaskGP(
+            torch.tensor( norm_df_train[['experiment_flag']+input_names].values ),
+            torch.tensor( norm_df_train[output_names].values ),
+            task_feature=0,
+            covar_module=ScaleKernel(MaternKernel(nu=1.5)),
+            outcome_transform=None,
+        )
+        cov = gp_model.task_covar_module._eval_covar_matrix()
+        print( 'Correlation: ', cov[1,0]/torch.sqrt(cov[0,0]*cov[1,1]).item() )
+
+    else:
+        gp_model = SingleTaskGP(
+            torch.tensor(norm_df_train[input_names].values, dtype=torch.float64),
+            torch.tensor(norm_df_train[output_names].values, dtype=torch.float64),
+            covar_module=ScaleKernel(MaternKernel(nu=1.5)),
+            outcome_transform=None,
+        )
+
+    # Fit the model
+    mll = ExactMarginalLogLikelihood(gp_model.likelihood, gp_model)
+    fit_gpytorch_mll(mll)
+
+    # Fix mismatch in name between the config file and the expected lume-model format
+    for k in input_variables:
+        print(input_variables[k])
+        input_variables[k]['default_value'] = input_variables[k]['default']
+        del input_variables[k]['default']
+
+    input_variables = [ ScalarVariable(**input_variables[k]) for k in input_variables.keys() ]
+
+    if experiment != 'acave':
+        output_variables = [
+            DistributionVariable(name=f"{name}_{suffix}", distribution_type="MultiVariateNormal")
+            for name in output_names
+            for suffix in ["sim_task", "exp_task"]
+        ]
+    else:
+        output_variables = [
+            DistributionVariable(name=f"{name}_{suffix}", distribution_type="MultiVariateNormal")
+            for name in output_names
+            for suffix in ["sim_task"]
+        ]
+    #Save GP model
+    model = GPModel(
+        model=gp_model,
+        input_variables=input_variables,
+        output_variables=output_variables,
+        input_transformers=[input_transform],
+        output_transformers=[output_transform],
+    )
+
+    path_to_save = path_to_IFE_sf_src+'/ml/saved_models/GP_training/'
+    model.dump( file=os.path.join(path_to_save, experiment+'.yml'), save_models=True )
+    print(f"Model saved to {path_to_save}")
 
 # Upload the model to the database
-model_choice = 'NN'
 # - Load the files that were just created into a dictionary
 print(f"Loading model from {path_to_save}")
 with open(os.path.join(path_to_save, experiment+'.yml')) as f:
     yaml_file_content = f.read()
 document = {
     'experiment': experiment,
-    'model_type': model_choice,
+    'model_type': model_type,
     'yaml_file_content': yaml_file_content
 }
 print(document)
@@ -177,11 +298,11 @@ for filename in [ model_info['model'] ] + model_info['input_transformers'] + mod
     with open(os.path.join(path_to_save, filename), 'rb') as f:
         document[filename] = f.read()
 # - Check whether there is already a model in the database
-query = {'experiment': experiment, 'model_type': model_choice}
+query = {'experiment': experiment, 'model_type': model_type}
 count = db['models'].count_documents(query)
 # - Upload/replace the model in the database
 if count > 1:
-    print(f"Multiple models found for experiment: {experiment} and model type: {model_choice}! Removing them.")
+    print(f"Multiple models found for experiment: {experiment} and model type: {model_type}! Removing them.")
     db['models'].delete_many(query)
     count = db['models'].count_documents(query)
 if count == 0:
@@ -193,6 +314,6 @@ elif count == 1:
     db['models'].update_one(query, {'$set': document})
 else:
     # Raise error, this should not happen
-    raise ValueError(f"Multiple models found for experiment: {experiment} and model type: {model_choice}!")
+    raise ValueError(f"Multiple models found for experiment: {experiment} and model type: {model_type}!")
 
 print("Model updated in database")
