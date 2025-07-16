64238dfc7ddbe43e948c2e67b6f8c55e3dbca2f0
Remi Lehe <remi.lehe@normalesup.org>
2025-07-16 09:55:36 +0200

    Add option to run with specified parameters

--- simulation_data/acave/templates/run_grid_scan.py
+++ simulation_data/acave/templates/run_grid_scan.py
@@ -1,24 +1,43 @@
 """ Run parameter scan for A-cave experiment """
 
-from optimas.core import Parameter, VaryingParameter, Objective
+from optimas.core import VaryingParameter, Objective
 from optimas.generators import GridSamplingGenerator
-from optimas.evaluators import TemplateEvaluator,FunctionEvaluator,ChainEvaluator
+from optimas.evaluators import TemplateEvaluator,ChainEvaluator
 from optimas.explorations import Exploration
+import pandas as pd
+import argparse
+
+# Parse arguments to determine whether to run a single simulation or a grid scan
+parser = argparse.ArgumentParser()
+parser.add_argument('--single-simulation-parameters', default=None,
+    help='Path to the CSV file containing simulation parameters ; if not provided, a grid scan will be run')
+args = parser.parse_args()
+
+if args.single_simulation_parameters is None:
+    # Grid scan with parallel simulations
+    n_var_1 = 5
+    n_var_2 = 5
+    sim_workers = 5
+    var_1 = VaryingParameter("target_to_focus_distance", -0.1, 0.1)
+    var_2 = VaryingParameter("fused_silica_thickness", -667, 667)
+else:
+    # Single simulation, with parameters provided in the CSV file
+    n_var_1 = 1
+    n_var_2 = 1
+    sim_workers = 1
+    # Extract parameters from the CSV file
+    df = pd.read_csv(args.single_simulation_parameters)
+    # Set the varying parameters with the same min and max value,
+    # so that the simulation is run exactly with the provided parameters
+    var_1 = VaryingParameter("target_to_focus_distance", df["target_to_focus_distance"].iloc[0], df["target_to_focus_distance"].iloc[0])
+    var_2 = VaryingParameter("fused_silica_thickness", df["fused_silica_thickness"].iloc[0], df["fused_silica_thickness"].iloc[0])
 
 # Specify the analysis function.
 def analysis_func_main(work_dir, output_params):
     output_params['f'] = 0
 
 # Create varying parameters and objectives.
-var_1 = VaryingParameter("target_to_focus_distance", -0.1, 0.1)
-var_2 = VaryingParameter("fused_silica_thickness", -667, 667)
 obj = Objective("f", minimize=False)
-
-n_var_1 = 5
-n_var_2 = 5
-sim_workers = 1
-
-n_total = n_var_1 * n_var_2
 
 # Create generator.
 gen = GridSamplingGenerator(
@@ -56,7 +75,7 @@ ev_chain = ChainEvaluator(
 exp = Exploration(
     generator=gen,
     evaluator=ev_chain,
-    max_evals=n_total,
+    max_evals=n_var_1 * n_var_2,
     sim_workers=sim_workers,
     run_async=True,  # with the GridSamplingGenerator it should not matter if we run in batches,
 )
