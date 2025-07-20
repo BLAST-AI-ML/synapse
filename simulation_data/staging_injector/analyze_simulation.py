c74d5ea51749f3b6f063849f04d33b4fa4a9f59e
Remi Lehe <remi.lehe@normalesup.org>
2025-07-20 08:12:20 +0200

    Update analysis script

--- simulation_data/staging_injector/analyze_simulation.py
+++ simulation_data/staging_injector/analyze_simulation.py
@@ -22,7 +22,7 @@ except ImportError:
 # General parameters of the analysis
 pol = 'y'
 uz_threshold = 10
-    
+
 def analyze_simulation():
 
     # Get current directory
@@ -30,11 +30,12 @@ def analyze_simulation():
     ts = LpaDiagnostics( os.path.join(data_directory, 'diags/diag') )
 
     # Load input parameters
-    # TODO: load
-#    with open('input_params.json') as file:
-#        data = json.load(file)
-    data = {}
-    
+    # TODO: template this script
+    data["Laser energy [J]"] = 12
+    data["Target-to-focus distance [cm]"] = 1
+    data["Dopant concentration [%]"] = 5
+    data["Background density [1e18/cm^3]"] = 4
+
     # Additional metadata
     data['experiment_flag'] = 0
     data['date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -45,28 +46,19 @@ def analyze_simulation():
         text = f.read()
         stage_length = float( re.findall(r'my_constants\.stage_length = (.+)', text)[0] )
         ramp_length = float( re.findall(r'my_constants\.ramp_length = (.+)', text)[0] )
-        dopant_length = float( re.findall(r'my_constants\.dopant_length = (.+)', text)[0] )    
+        dopant_length = float( re.findall(r'my_constants\.dopant_length = (.+)', text)[0] )
         dopant_fraction = float( re.findall(r'my_constants\.dopant_fraction = (.+)', text)[0] )
         n_atom = float( re.findall(r'my_constants\.n_atom = (.+)', text)[0] )
         hydrogen_density_function = re.findall(r'hydrogen1\.density_function\(x,y,z\) = (.+)', text)[0]
         nitrogen_density_function = re.findall(r'nitrogen1\.density_function\(x,y,z\) = (.+)', text)[0]
-    
+
     # Compute average wavelength at the last iteration and add it to the data
     S, info = ts.get_laser_spectral_intensity(
         iteration=ts.iterations[-1], pol='x')
     lambda_avg = np.average( 2*np.pi/info.k[1:], weights=S[1:] )
-    data['mean_wavelength'] = lambda_avg*1e9 # convert from m to nm
+    data['Mean laser wavelength [nm]'] = lambda_avg*1e9 # convert from m to nm
 
-    # Write to the data base
-    db = pymongo.MongoClient(
-        host="mongodb05.nersc.gov",
-        username="bella_sf_admin",
-        password=os.getenv("SF_DB_ADMIN_PASSWORD"),
-        authSource="bella_sf")["bella_sf"]
-    collection = db["staging_injector"]
-    collection.insert_one(data)
-
-    # Create plots of the interation
+    # Create plots of the interaction
 
     # First compute quantities across the interaction:
     # - Density
@@ -83,21 +75,34 @@ def analyze_simulation():
         return np.average( info.z, weights=env )
     z_laser = ts.iterate( get_mean_laser_position )
     # - beam charge
-    Q = ts.iterate( ts.get_charge, species='electrons_n1', 
+    Q = ts.iterate( ts.get_charge, species='electrons_n1',
                    select={'uz':[uz_threshold, None]})
     gamma, dgamma = ts.iterate( ts.get_mean_gamma, species='electrons_n1')
-    
+
+    data['Beam mean energy [GeV]'] = gamma[-1]*0.511e-3 # convert from m to nm
+    data['Beam energy spread [%]'] = dgamma[-1]/gamma[-1]
+    data['Trapped charge [pC]'] = Q[-1]*1e12
+
+    # Write to the data base
+    db = pymongo.MongoClient(
+        host="mongodb05.nersc.gov",
+        username="bella_sf_admin",
+        password=os.getenv("SF_DB_ADMIN_PASSWORD"),
+        authSource="bella_sf")["bella_sf"]
+    collection = db["staging_injector"]
+    collection.insert_one(data)
+
     def visualize_iteration(iteration):
-    
+
         # Prepare figure
         plt.clf()
         fig = plt.gcf()
         gs = GridSpec(4, 2)
-        
+
         # find laser position at the current iteration
         current_z_las = z_laser[ np.argmin(abs(ts.iterations-iteration)) ]
         ct = c*ts.t[abs(iteration-ts.iterations).argmin()]
-        
+
          # Plot of density as a function of z
         fig.add_subplot(gs[0,0])
         plt.plot( 1e2*z, 1e-6*n_H, label='Hydrogen' )
@@ -107,7 +112,7 @@ def analyze_simulation():
         plt.grid()
         plt.axvline(x=1e2*current_z_las, color='k', ls='--')
         plt.xlim(0, 1e2*stage_length)
-    
+
         # Plot of charge
         fig.add_subplot(gs[1,0])
         plt.plot( 1e2*z_laser, -1e12*Q, color='b' )
@@ -115,17 +120,17 @@ def analyze_simulation():
         plt.grid()
         plt.axvline(x=1e2*current_z_las, color='k', ls='--')
         plt.xlim(0, 1e2*stage_length)
-        
+
         # Plot of energy and energy spread
         fig.add_subplot(gs[2,0])
         plt.plot( 1e2*z_laser, 0.511e-3*gamma, color='b' )
-        plt.fill_between( 1e2*z_laser, 0.511e-3*(gamma-dgamma), 
+        plt.fill_between( 1e2*z_laser, 0.511e-3*(gamma-dgamma),
                          0.511e-3*(gamma-dgamma), color='b', alpha=0.3)
         plt.ylabel('Beam energy [GeV]')
         plt.grid()
         plt.axvline(x=1e2*current_z_las, color='k', ls='--')
         plt.xlim(0, 1e2*stage_length)
-        
+
         # Plot of a0 and w0 as a function of z
         fig.add_subplot(gs[3,0])
         plt.plot( 1e2*z_laser, a0, 'r' )
@@ -137,37 +142,37 @@ def analyze_simulation():
         plt.ylabel(r'waist [$\mu$m]', color='purple')
         plt.axvline(x=1e2*current_z_las, color='k', ls='--')
         plt.xlim(0, 1e2*stage_length)
-    
+
         # Plot of the laser + accelerating field
         fig.add_subplot(gs[0:2,1])
         Ez, info = ts.get_field('E', 'z', iteration=iteration)
         plt.imshow( abs(Ez.T), cmap='Blues', interpolation='bicubic',
-                extent=[1e6*(info.zmin-ct), 1e6*(info.zmax-ct), 
+                extent=[1e6*(info.zmin-ct), 1e6*(info.zmax-ct),
                         1e6*info.rmin, 1e6*info.rmax],
                 aspect='auto', vmin=0, vmax=5e10)
         plt.ylim(-100, 100)
         env, info = ts.get_laser_envelope(iteration=iteration, pol=pol)
-        transp_imshow(abs(env.T), cmap='gist_heat_r', 
+        transp_imshow(abs(env.T), cmap='gist_heat_r',
             aspect='auto', gam=0.3,
-            extent=[1e6*(info.zmin-ct), 1e6*(info.zmax-ct), 
+            extent=[1e6*(info.zmin-ct), 1e6*(info.zmax-ct),
                     1e6*info.rmin, 1e6*info.rmax] )
-        xp, zp = ts.get_particle(['x', 'z'], iteration=iteration, 
+        xp, zp = ts.get_particle(['x', 'z'], iteration=iteration,
             species='electrons_n1')
         plt.plot(1e6*(zp-ct), 1e6*xp, '.', color='orange', ms=1 )
         plt.xlabel(r'$z - ct \;[\mu m]$')
         plt.ylabel(r'$x [\mu m]$')
         plt.title(r'$|E_z|$ (blue), laser envelope (red)')
         plt.xlim(-95, 0)
-    
+
         # Plot of the electron energy spectrum
         fig.add_subplot(gs[2,1])
-        uz, w = ts.get_particle(['uz', 'w'], iteration=iteration, 
+        uz, w = ts.get_particle(['uz', 'w'], iteration=iteration,
             select={'uz':[uz_threshold, None]}, species='electrons_n1')
-        plt.hist( 0.511e-3*uz, weights=w, bins=200, 
+        plt.hist( 0.511e-3*uz, weights=w, bins=200,
                  range=[0, 1.2*0.511e-3*np.nanmax(gamma)] )
         plt.xlabel(r'Energy [GeV]')
         plt.title('Electron energy spectrum')
-        
+
         # Plot of the laser spectrum
         # TODO: Use proper function
         fig.add_subplot(gs[3,1])
@@ -177,7 +182,7 @@ def analyze_simulation():
         plt.title('Laser spectrum')
         plt.plot( 1.e6*lambd, 1e3*S[1:], color='r' )
         plt.xlim(0.5,1.2)
-        
+
         plt.subplots_adjust(hspace=0.5)
         plt.savefig('diags/plots/iteration_%05d.png' % iteration)
 
@@ -187,7 +192,7 @@ def analyze_simulation():
     # Create images
     plt.figure(figsize=(12, 12))
     ts.iterate( visualize_iteration )
-    
+
     # Load images and convert to MP4
     image_files = ['diags/plots/iteration_%05d.png' % iteration for iteration in ts.iterations]
     with imageio.get_writer('diags/plots/animation.mp4', fps=5) as writer:
