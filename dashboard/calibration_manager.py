1caeec7513f9555695c17308cd9c1393d25a7c6c
Ethan Rodriguez <erodriguez133@csit-8027950.dhcp.lbl.gov>
2025-07-22 13:08:47 -0700

    fix

--- dashboard/calibration_manager.py
+++ dashboard/calibration_manager.py
@@ -16,5 +16,4 @@ class SimulationCalibrationManager:
         for value in self.simulation_calibration.values():
             sim_name = value["name"]
             exp_name = value["depends_on"]
-            df_sim[exp_name] = df_sim[sim_name] / value["alpha"] + value["beta"]
-            
+            df_sim[exp_name] = df_sim[sim_name] / value["alpha"] + value["beta"]            
