65319df06527017be79c26f2b511564fb2e58111
Ethan Rodriguez <erodriguez133@csit-8027950.dhcp.lbl.gov>
2025-07-22 15:57:59 -0700

    take out print statements

--- dashboard/parameters_manager.py
+++ dashboard/parameters_manager.py
@@ -101,7 +101,7 @@ class ParametersManager:
         save_dir = f"../simulation_data/{setup}"
         data_df = pd.DataFrame(sim_data)
         data_df.to_csv(os.path.join(save_dir, "single_sim_vals.csv"), index=False)
-        
+        print("simulation values saved to csv")
         try:
             with Client(
                 client_id=state.sfapi_client_id, secret=state.sfapi_key
@@ -110,17 +110,14 @@ class ParametersManager:
                 # set the target path where auxiliary files will be copied
                 target_path1 = f"/global/cfs/cdirs/m558/superfacility/simulation_data/{setup}/"
                 [target_path1] = perlmutter.ls(target_path1, directory=True)
-                print("target1 made")
                 target_path2 = f"/global/cfs/cdirs/m558/superfacility/simulation_data/{setup}/templates"
                 [target_path2] = perlmutter.ls(target_path2, directory=True)
-                print("target2 made")
                 source_path = Path.cwd().parent
                 source_path_list = [
                     Path(source_path / f"simulation_data/{setup}/templates/run_grid_scan.py"),
                     Path(source_path / f"simulation_data/{setup}/templates/warpx_input_script"),
                     Path(source_path / f"simulation_data/{setup}/single_sim_vals.csv")
                 ]
-                print(source_path_list)
                 #copy auxiliary files to NERSC
                 count = 1
                 for path in source_path_list:
