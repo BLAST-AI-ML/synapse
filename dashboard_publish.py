20a949185fc8f7b432687a15e47a9f0b10b50238
Edoardo Zoni <59625522+EZoni@users.noreply.github.com>
2025-08-22 07:40:52 -0700

    Add utility script to publish the container (#239)

---
+++ dashboard_publish.py
@@ -0,0 +1,40 @@
+import subprocess
+
+
+def run(command: str, proceed: str):
+    if proceed in ["y"]:
+        try:
+            subprocess.run(
+                command,
+                check=True,
+                encoding="utf-8",
+                shell=True,
+                text=True,
+            )
+        except Exception as error:
+            print(error)
+    else:
+        print("Skipping...")
+
+
+if __name__ == "__main__":
+    # prune all existing images
+    proceed = input("\nPrune all existing images? [y/N] ")
+    command = "docker system prune -a -f"
+    run(command, proceed)
+
+    # build the new image
+    proceed = input("\nBuild new image? [y/N] ")
+    command = "docker build --platform linux/amd64 -t gui -f dashboard/Dockerfile ."
+    run(command, proceed)
+
+    # upload to the NERSC registry
+    proceed = input("\nPublish new image? [y/N] ")
+    command_list = [
+        "docker login registry.nersc.gov",
+        "docker tag gui:latest registry.nersc.gov/m558/superfacility/gui:latest",
+        "docker tag gui:latest registry.nersc.gov/m558/superfacility/gui:$(date '+%y.%m')",
+        "docker push -a registry.nersc.gov/m558/superfacility/gui",
+    ]
+    command = " && ".join(command_list)
+    run(command, proceed)
