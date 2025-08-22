9a6604677a7461d9ad10360cd3afadae84d46183
Edoardo Zoni <ezoni@lbl.gov>
2025-08-21 17:49:45 -0700

    Cleaning

--- dashboard_publish.py
+++ dashboard_publish.py
@@ -1,18 +1,18 @@
 import subprocess
 
 
-def run(cmd: str, proceed: bool = False):
+def run(command: str, proceed: bool = False):
     if proceed in ["y"]:
         try:
             subprocess.run(
-                cmd,
+                command,
                 check=True,
                 encoding="utf-8",
                 shell=True,
                 text=True,
             )
-        except Exception as msg:
-            print(msg)
+        except Exception as error:
+            print(error)
     else:
         print("Skipping...")
 
@@ -20,21 +20,21 @@ def run(cmd: str, proceed: bool = False):
 if __name__ == "__main__":
     # prune all existing images
     proceed = input("\nPrune all existing images? [y/N] ")
-    cmd = "docker system prune -a -f"
-    run(cmd, proceed)
+    command = "docker system prune -a -f"
+    run(command, proceed)
 
     # build the new image
     proceed = input("\nBuild new image? [y/N] ")
-    cmd = "docker build --platform linux/amd64 -t gui -f dashboard/Dockerfile ."
-    run(cmd, proceed)
+    command = "docker build --platform linux/amd64 -t gui -f dashboard/Dockerfile ."
+    run(command, proceed)
 
     # upload to the NERSC registry
     proceed = input("\nPublish new image? [y/N] ")
-    cmd_list = [
+    command_list = [
         "docker login registry.nersc.gov",
         "docker tag gui:latest registry.nersc.gov/m558/superfacility/gui:latest",
         "docker tag gui:latest registry.nersc.gov/m558/superfacility/gui:$(date '+%y.%m')",
         "docker push -a registry.nersc.gov/m558/superfacility/gui",
     ]
-    cmd = " && ".join(cmd_list)
-    run(cmd, proceed)
+    command = " && ".join(command_list)
+    run(command, proceed)
