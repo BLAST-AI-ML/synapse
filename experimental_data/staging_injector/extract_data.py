89c3e98e9230f03b4389f0f5a57ce25d1847da0c
Remi Lehe <remi.lehe@normalesup.org>
2025-08-27 09:46:41 -0700

    Extract experimental data for `staging_injector` (#244)

---
+++ experimental_data/staging_injector/extract_data.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+"""
+This script will watch for new scan files anywhere in the 08-Aug
+folder and uploads the experimental results to the database.
+
+This should be run with:
+
+python extract_data.py
+"""
+
+import time
+import re
+import os
+import pymongo
+from datetime import datetime
+import pandas as pd
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import numpy as np
+
+# LabVIEW epoch (1904-01-01) to Unix epoch (1970-01-01) offset in seconds
+LABVIEW_EPOCH_OFFSET = 2082844800
+
+# data to be extracted
+watched_folder = r'N:\data\Y2025\08-Aug'
+watched_pattern = r'25_08'
+experiment = 'staging_injector'
+data_to_extract = {
+    'Amplifier 3 [J]': 'EM-LB-2 Reading.Channel 2 Alias:Amplifier 3 [J]',
+    'HEX-PL1-1 xpos': 'HEX-PL1-1 xpos',
+    'Valve01': 'MANPAR-BELLA-ValveN2Frac value1 Alias:Valve01',
+    'Cap downstream (torr)': 'GAUGE-PL1-CapPressure pressure2 Alias:Cap downstream (torr)',
+    'EBeamPrf charge [pC]': 'EBeamPrf charge [pC]',
+}
+unavailable_data = [
+    "Beam mean energy [GeV]",
+    "Beam energy spread [%]",
+    "Mean laser wavelength [nm]",
+]
+
+def extract_info_more_scan_file( path_to_scan_file ):
+    # Open the scan file
+    s_file = pd.read_csv(path_to_scan_file, sep='\t')
+
+    # Extract the scan number from the file name
+    m = re.search(r's(\d+)\.txt', path_to_scan_file)
+    scan_number = int(m.groups(1)[0])
+
+    # Check for required columns
+    missing_column = False
+    for _, value in data_to_extract.items():
+        if value not in s_file.columns:
+            print(f"Warning: Column '{value}' not found in {path_to_scan_file}")
+            missing_column = True
+
+    if not missing_column:
+        # Loop over shots
+        for i in range(len(s_file)):
+            # Extract data for each shot
+            data = {}
+            data['experiment_flag'] = 1
+            data['scan_number'] = scan_number
+            data['shot_number'] = i
+            # Convert LabVIEW timestamp to Unix timestamp by subtracting the offset
+            labview_timestamp = s_file['DateTime Timestamp'].iloc[i]
+            unix_timestamp = labview_timestamp - LABVIEW_EPOCH_OFFSET
+            data['date'] = datetime.fromtimestamp(unix_timestamp).strftime("%Y-%m-%d %H:%M:%S")
+
+            # For now, use the date as a unique identifier:
+            # if there is another shot in the database with the same date, skip it
+            if collection.find_one({'date': data['date'], 'experiment_flag': 1}):
+                print(f"Skipping shot {i} because it already exists in the database")
+                continue
+
+            # Set unavailable data to NaN
+            for key in unavailable_data:
+                data[key] = np.nan
+
+            # Extract required data
+            for key, value in data_to_extract.items():
+                data[key] = s_file[value].iloc[i]
+
+            # Add to the database
+            print('Uploading: ', data)
+            collection.insert_one(data)
+
+class MyHandler(FileSystemEventHandler):
+
+    def __init__(self):
+        # Compile the regex pattern once for efficiency
+        # (Checks for new scan files, extracts scan number)
+        self.pattern = re.compile(watched_pattern + r'\d\d\\analysis\\s\d+\.txt')
+
+    def on_created(self, event):
+        # Check if a new scan file has been created
+        print(event.src_path)
+        if self.pattern.search(event.src_path):
+            print(f"New matching file created: {event.src_path}")
+            extract_info_more_scan_file(event.src_path)
+
+
+if __name__ == '__main__':
+
+    # Open credential file for database
+    with open('C:/Users/rlehe.BELLAAPPSERVER/Documents/db.profile') as f:
+        db_profile = f.read()
+    # Connect to the MongoDB database with read-only access
+    db = pymongo.MongoClient(
+        host="mongodb05.nersc.gov",
+        username="bella_sf_admin",
+        password=re.findall('SF_DB_ADMIN_PASSWORD=(.+)', db_profile)[0],
+        authSource="bella_sf")["bella_sf"]
+    collection = db[experiment]
+
+    # Upload existing data in the database
+    folders_to_upload = [
+        os.path.join(watched_folder, r'25_0826\analysis'),
+    ]
+    for folder_to_upload in folders_to_upload:
+        for filename in os.listdir(folder_to_upload):
+            print(filename)
+            if re.match(r's\d+.txt',  filename ):
+                extract_info_more_scan_file( os.path.join(folder_to_upload, filename) )
+
+    # Create an observer and handler
+    observer = Observer()
+    event_handler = MyHandler()
+
+    # Schedule the observer to watch the directory
+    observer.schedule(event_handler, path=watched_folder, recursive=True)
+    observer.start()
+
+    # Keep the script running
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
