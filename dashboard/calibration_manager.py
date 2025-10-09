988c99c8d2401bdf0de5aa193ad5acff69b52719
Remi Lehe <remi.lehe@normalesup.org>
2025-10-09 11:08:04 -0700

    Add calibration panel (#254)
    Co-authored-by: adhamrait <adhamrait@lbl.gov>

--- dashboard/calibration_manager.py
+++ dashboard/calibration_manager.py
@@ -1,13 +1,60 @@
+from trame.widgets import client, vuetify3 as vuetify, html
+from state_manager import state
+import copy
+
+
 class SimulationCalibrationManager:
     def __init__(self, simulation_calibration):
-        self.simulation_calibration = simulation_calibration
+        state.simulation_calibration = copy.deepcopy(simulation_calibration)
 
     def convert_sim_to_exp(self, df_sim):
         """
         Apply calibration to the simulation points, so as to reconstruct
         the same input/output variables as the experimental points
         """
-        for value in self.simulation_calibration.values():
+        for value in state.simulation_calibration.values():
             sim_name = value["name"]
             exp_name = value["depends_on"]
             df_sim[exp_name] = df_sim[sim_name] / value["alpha"] + value["beta"]
+
+    def panel(self):
+        print("Setting calibration card...")
+        with vuetify.VExpansionPanels(v_model=("expand_panel_control_calibration", 0)):
+            with vuetify.VExpansionPanel(title="Control: Calibrate simulation points"):
+                with vuetify.VExpansionPanelText():
+                    with client.DeepReactive("simulation_calibration"):
+                        for key in state.simulation_calibration.keys():
+                            # create a row for the parameter label
+                            with vuetify.VRow():
+                                vuetify.VListSubheader(
+                                    state.simulation_calibration[key]["name"]
+                                )
+                            with vuetify.VRow():
+                                html.Small(
+                                    f"= α × ({state.simulation_calibration[key]['depends_on']} - β)"
+                                )
+                            with vuetify.VRow():
+                                with vuetify.VCol():
+                                    vuetify.VTextField(
+                                        v_model_number=(
+                                            f"simulation_calibration['{key}']['alpha']",
+                                        ),
+                                        change="flushState('simulation_calibration')",
+                                        density="compact",
+                                        hide_details=True,
+                                        style="width: 100px;",
+                                        type="number",
+                                        label="α",
+                                    )
+                                with vuetify.VCol():
+                                    vuetify.VTextField(
+                                        v_model_number=(
+                                            f"simulation_calibration['{key}']['beta']",
+                                        ),
+                                        change="flushState('simulation_calibration')",
+                                        density="compact",
+                                        hide_details=True,
+                                        style="width: 100px;",
+                                        type="number",
+                                        label="β",
+                                    )
