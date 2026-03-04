dca222ebbc89d43c6d711bf92e82f45fd992cce1
Remi Lehe <remi.lehe@normalesup.org>
2026-03-04 15:47:56 -0800

    DB credential: read environment variable rather than `db.profile` (#377)

--- ml/train_model.py
+++ ml/train_model.py
@@ -14,7 +14,6 @@ from botorch.models import MultiTaskGP, SingleTaskGP, ModelListGP
 from botorch.fit import fit_gpytorch_mll
 from gpytorch.kernels import ScaleKernel, MaternKernel
 import pymongo
-import re
 import yaml
 import mlflow
 from lume_model.models import TorchModel
@@ -89,13 +88,9 @@ def connect_to_db(config_dict):
     db_auth = config_dict["database"]["auth"]
     db_username = config_dict["database"]["username_ro"]
     db_password_env = config_dict["database"]["password_ro_env"]
-    # Look for the password in the profile file
-    with open(os.path.join(os.getenv("HOME"), "db.profile")) as f:
-        db_profile = f.read()
-    match = re.search(f"{db_password_env}='([^']*)'", db_profile)
-    if not match:
-        raise RuntimeError(f"Environment variable {db_password_env} must be set")
-    db_password = match.group(1)
+    db_password = os.getenv(db_password_env)
+    if db_password is None:
+        raise RuntimeError(f"Environment variable {db_password_env} must be set!")
     return pymongo.MongoClient(
         host=db_host,
         authSource=db_auth,
