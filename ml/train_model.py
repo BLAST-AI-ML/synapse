178a876b356da6c37cb3ce62f3af4e7e857bdfcc
Remi Lehe <remi.lehe@normalesup.org>
2026-03-05 13:53:04 -0800

    Apply suggestions from code review

--- ml/train_model.py
+++ ml/train_model.py
@@ -240,6 +240,7 @@ def build_lume_model(
         ]
 
     # Create lume-model objects
+    # Create lume-model objects
     if model_type == "GP":
         return GPModel(
             model=model.cpu(),
