a4d752fdf39286f372623e7ecad82129354d95e8
Remi Lehe <remi.lehe@normalesup.org>
2026-03-21 07:41:38 -0700

    Better error message

--- dashboard/model_manager.py
+++ dashboard/model_manager.py
@@ -75,7 +75,7 @@ class ModelManager:
 
         if "mlflow" not in config_dict or not config_dict["mlflow"].get("tracking_uri"):
             print(
-                "No mlflow.tracking_uri in configuration file; cannot load model from MLflow."
+                f"No mlflow.tracking_uri in configuration file for {config_dict['experiment']}; cannot load model from MLflow."
             )
             return
 
