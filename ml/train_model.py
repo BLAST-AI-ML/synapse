b207e616ad0b10d2ac981392a9b96135ab6c3ca6
Remi Lehe <remi.lehe@normalesup.org>
2026-03-24 20:39:04 -0700

    Fix test

--- ml/train_model.py
+++ ml/train_model.py
@@ -153,31 +153,39 @@ def build_guess_calibration(config_dict, input_variables, output_variables):
     # untransform gives: exp = sim / alpha + beta.
     simulation_calibration = config_dict.get("simulation_calibration", {})
 
+    # Build lookup from experimental variable name (depends_on) to calibration entry.
+    # This mirrors the dashboard approach: calibration entries are matched by depends_on,
+    # not by key, so entries keyed "input6" can map an output variable just as well as
+    # entries keyed "output1".
+    depends_on_lookup = {
+        entry["depends_on"]: entry
+        for entry in simulation_calibration.values()
+        if "depends_on" in entry
+    }
+
+    def _get_calibration(exp_name):
+        if exp_name in depends_on_lookup:
+            entry = depends_on_lookup[exp_name]
+            return entry["name"], entry["alpha_guess"], entry["beta_guess"]
+        return exp_name, 1.0, 0.0
+
     sim_input_names = []
     alpha_input_list = []
     beta_input_list = []
     for key in input_variables:
-        if key in simulation_calibration:
-            sim_input_names.append(simulation_calibration[key]["name"])
-            alpha_input_list.append(simulation_calibration[key]["alpha_guess"])
-            beta_input_list.append(simulation_calibration[key]["beta_guess"])
-        else:
-            sim_input_names.append(input_variables[key]["name"])
-            alpha_input_list.append(1.0)
-            beta_input_list.append(0.0)
+        sim_name, alpha, beta = _get_calibration(input_variables[key]["name"])
+        sim_input_names.append(sim_name)
+        alpha_input_list.append(alpha)
+        beta_input_list.append(beta)
 
     sim_output_names = []
     alpha_output_list = []
     beta_output_list = []
     for key in output_variables:
-        if key in simulation_calibration:
-            sim_output_names.append(simulation_calibration[key]["name"])
-            alpha_output_list.append(simulation_calibration[key]["alpha_guess"])
-            beta_output_list.append(simulation_calibration[key]["beta_guess"])
-        else:
-            sim_output_names.append(output_variables[key]["name"])
-            alpha_output_list.append(1.0)
-            beta_output_list.append(0.0)
+        sim_name, alpha, beta = _get_calibration(output_variables[key]["name"])
+        sim_output_names.append(sim_name)
+        alpha_output_list.append(alpha)
+        beta_output_list.append(beta)
 
     alpha_inputs = torch.tensor(alpha_input_list, dtype=torch.float)
     beta_inputs = torch.tensor(beta_input_list, dtype=torch.float)
